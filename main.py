d75d72bed656614ef258049ccad36e4b8b88225c
Rohan Chopra <09rohanchopra@gmail.com>
2017-03-12 06:23:33 +0530

    Made visual changes
    * Added second screen's basic UI
    * Other visual changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from PyQt4 import QtGui,uic 
-import sys 
+from PyQt4 import QtGui,uic,QtCore
+import sys, os
 import cv2
-import os
+import ctypes
 
 # get the directory of this script
 path = os.path.dirname(os.path.abspath(__file__))
@@ -10,28 +10,34 @@ MainWindowUI, MainWindowBase = uic.loadUiType(
     os.path.join(path, 'gui.ui'))
 
 
+myappid = 'VeSee.scanner.0.01' 
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 class VeSeeApp(MainWindowBase, MainWindowUI):
     def __init__(self):
         
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        
-        self.startProg.clicked.connect(self.start_video)  
+        self.startButton.clicked.connect(self.start_video)
+
+        self.analysingLabel.setVisible(False)
+        self.logoLabel.setPixmap(QtGui.QPixmap("logo.png"))
+        self.logoLabel.setAlignment(QtCore.Qt.AlignCenter)
                                                             
     def start_video(self):
         print('Clicked!')
-        camera = cv2.VideoCapture(1)
-        while True:
-           f,img = camera.read()
-           cv2.imshow("webcam",img)
-           if (cv2.waitKey(5) != -1):
-               break
-        video.release()
+        self.startButton.setVisible(False)
+        self.buttonFrame.setVisible(False)
+        self.analysingLabel.setVisible(True)
+        #Function for camera
+        #Function for IR
+
+        
 
 
 def main():
-    app = QtGui.QApplication(sys.argv)  
+    app = QtGui.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon('logo.png'))
     detector = VeSeeApp()  
     detector.show()  
     app.exec_()  
